3a66c61147d7dca62ebf67cb81dbe124f7c6a920
yuri <yuri.yoshida@forlogic.net>
2023-11-28 10:21:13 -0300

    parte yuri
    tabela, armazenamento, localstorage e excluir

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,78 +1,84 @@
-let myNodelist = document.getElementsByTagName("LI");
-let i;
-for (i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-let list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
 function newElement() {
-  debugger
-  var nome = document.getElementById('nameInput').value;
-  var email = document.getElementById('mailInput').value;
+  let name = document.getElementById('nameInput').value;
+  let mail = document.getElementById('mailInput').value;
+  let cpf = document.getElementById('CPFInput').value;
 
-  if (nome && email) {
-  var formData = {
-      nome: nome,
-      email: email
+
+  if (name && cpf && mail && mail.indexOf('@') !== -1) {
+  let formData = {
+      name: name,
+      mail: mail
     };
-    var table = document.getElementById('dataTable');
-    var tbody = table.getElementsByTagName('tbody')[0];
+    let table = document.getElementById('dataTable');
+    let tbody = table.getElementsByTagName('tbody')[0];
   
-    var newRow = tbody.insertRow(tbody.rows.length);
+    let newRow = tbody.insertRow(tbody.rows.length);
   
-    var cellName = newRow.insertCell(0);
-    var cellMail = newRow.insertCell(1);
-    cellName.innerHTML = formData.nome;
-    cellMail.innerHTML = formData.email;
-    var storedData = JSON.parse(localStorage.getItem('formData')) || [];
+    let cellName = newRow.insertCell(0);
+    let cellMail = newRow.insertCell(1);
+    let cellActions = newRow.insertCell(2);
+    cellName.innerHTML = formData.name;
+    cellMail.innerHTML = formData.mail;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'X';
+    deleteButton.onclick = function() {
+      deleteRow(newRow.rowIndex);
+    };
+    cellActions.appendChild(deleteButton);
+
+    let storedData = JSON.parse(localStorage.getItem('formData')) || [];
     storedData.push(formData);
     localStorage.setItem('formData', JSON.stringify(storedData));
   } else {
-    alert('Por favor, preencha todos os campos do formulário.');
+    alert('Por favor, preencha os campos de nome, email e CPF corretamente.');
+  }
+}
+
+window.onload = function () {
+  let storedData = JSON.parse(localStorage.getItem('formData')) || [];
+  let table = document.getElementById('dataTable');
+  let tbody = table.getElementsByTagName('tbody')[0];
+
+  storedData.forEach(function(data) {
+    let newRow = tbody.insertRow(tbody.rows.length);
+    let cellName = newRow.insertCell(0);
+    let cellMail = newRow.insertCell(1);
+    let cellActions = newRow.insertCell(2);
+
+    cellName.innerHTML = data.name;
+    cellMail.innerHTML = data.mail;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'X';
+    deleteButton.onclick = function() {
+      deleteRow(newRow.rowIndex);
+    };
+    cellActions.appendChild(deleteButton);
+  });
+};
+
+function deleteRow(rowIndex) {
+  let table = document.getElementById('dataTable');
+  table.deleteRow(rowIndex);
+
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  let table = document.getElementById('dataTable');
+  let tbody = table.getElementsByTagName('tbody')[0];
+  let rows = tbody.getElementsByTagName('tr');
+  let storedData = [];
+
+  for (let i = 0; i < rows.length; i++) {
+    let cells = rows[i].getElementsByTagName('td');
+    let formData = {
+      name: cells[0].innerHTML,
+      mail: cells[1].innerHTML
+    };
+    storedData.push(formData);
   }
 
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("nameInput").value;
-  let t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("entryList").appendChild(li);
-  }
-  document.getElementById("nameInput").value = "";
-
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  localStorage.setItem('formData', JSON.stringify(storedData));
 }
